abc688651168f288e6c2b3aeae7317001d2c74ba
Rex Ye <rxye1991@gmail.com>
2020-04-24 19:17:45 -0400

    gettingCurrentRoomObjectonChatroomcontentpagedone

--- src/Components/Chatrooms/ChatroomContent/ChatroomContent.jsx
+++ src/Components/Chatrooms/ChatroomContent/ChatroomContent.jsx
@@ -2,18 +2,25 @@ import React, { Component } from 'react'
 import './ChatroomContent.css'
 import {withRouter} from 'react-router-dom'
 export class ChatroomContent extends Component {
-
+    state={
+        currentRoom:{}
+    }
     componentDidMount(){
         //get current room info
         const currentRoomId = this.props.match.params.id
-        fetch(``)
+        fetch(`http://localhost:3000/rooms/${currentRoomId}`)
+        .then(res => res.json())
+        .then(currentRoom => {
+                this.setState({
+                    currentRoom
+                })
+        })
     }
-
 
     render() {
         return (
             <div>
-                <h5>Welcome to the chatroom</h5>
+                <h5>Welcome to the {this.state.currentRoom.name} chatroom</h5>
             </div>
         )
     }
