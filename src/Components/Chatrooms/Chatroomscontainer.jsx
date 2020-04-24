ff56d97de7da408e5bd844e871fef7a977f28804
Rex Ye <rxye1991@gmail.com>
2020-04-24 18:00:01 -0400

    fetchedChatrooms

--- src/Components/Chatrooms/Chatroomscontainer.jsx
+++ src/Components/Chatrooms/Chatroomscontainer.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from 'react'
 import './Chatroomscontainer.css'
+import Chatcontainer from '../Chatrooms/Chatcontainer/Chatcontainer'
 
 export class Chatroomscontainer extends Component {
     render() {
-        console.log(this.props)
+        const {chatRooms} = this.props
+        const chatRoomsMapper = chatRooms.map(chatRoom => {
+            return (
+                <Chatcontainer chatRoom={chatRoom}/>
+            )
+        })
         return (
             <div className='chatroomscontainer'>
                     <h5>All Rooms:</h5>
                     {/* Now render each room component with a loop  */}
-                    
+                    {chatRoomsMapper}
             </div>
         )
     }
